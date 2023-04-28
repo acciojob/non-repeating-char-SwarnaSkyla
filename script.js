c35012fd474302f8255183792e870f9069084eab
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-04-28 15:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+function nonRepeating(str){
+	const char={};
+	//count frequency
+	for(let i=;i<str.length;i++){
+		const c=str[i];
+		char[c]=char[c]?char[c]+1:1;
+	}
+
+	for(let i=0;i<str.length;i++){
+		const chara=str[i];
+		if(char[chara] === 1){
+			return chara;
+		}
+	}
+	return null;
+}
+const input=prompt("Enter a string");
+const firstnonrepeat=nonRepeating(input);
+
+if(firstnonrepeat){
+	alert ("$[firstnonrepeat]")
+}
+else{
+	alert("there is no repeated character");
+}
